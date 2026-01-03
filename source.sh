9df0209e78a7de506b998ab0a55b679205ae5aa3
Alix ANNERAUD <alix@anneraud.fr>
2026-01-03 13:06:23 +0100

    Replace inventory.ini with inventory.yml and update source.sh to reference the new inventory file

--- source.sh
+++ source.sh
@@ -1,3 +1,3 @@
 export ANSIBLE_VAULT_PASSWORD_FILE="$(pwd)/vault/.vault_password"
-export ANSIBLE_INVENTORY="$(pwd)/inventory.ini"
+export ANSIBLE_INVENTORY="$(pwd)/inventory.yml"
 export EDITOR="codium --wait"
